5125194fc850f6b1a6e7c85f96039358a61bab5c
Onke-M <onmweza@gmail.com>
2023-03-16 09:38:36 +0200

    Added CORS

--- flight-service/src/main/java/entelect/training/incubator/spring/flight/config/SecurityConfig.java
+++ flight-service/src/main/java/entelect/training/incubator/spring/flight/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package entelect.training.incubator.spring.flight.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
